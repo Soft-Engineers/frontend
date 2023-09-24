4c05b9af4b2294ffe6b16c87ad5ec3bcbc7637bc
mrbanana24 <franpercivaldi@gmail.com>
2023-09-24 18:36:06 -0300

    Button component implementation

---
+++ la_cosa_app/src/components/Button/index.jsx
@@ -0,0 +1,19 @@
+import Button from '@mui/material/Button';
+import Stack from '@mui/material/Stack';
+
+// Regular Button
+const RButton = ({text, action, icon}) => {
+  return (
+    <Stack direction="row" spacing={2}>
+      <Button 
+        variant="outlined" 
+        endIcon={icon}
+        onClick={action}
+      >
+        {text}
+      </Button>
+    </Stack>
+  );
+}
+
+export default RButton;
