651d6abe02a0b82a47108599dca5674560a53c75
Br1dam <brunodam665@gmail.com>
2023-10-11 13:06:17 -0300

    Nuevos event handlers sockets

--- la_cosa_app/src/components/Carta/index.jsx
+++ la_cosa_app/src/components/Carta/index.jsx
@@ -1,11 +1,15 @@
 import Stack from '@mui/material/Stack';
 import {backdropClasses, Card} from '@mui/material';
 import backImage from '../../assets/cartas_recortadas/back.png';
-import lanzallamaImage from '../../assets/cartas_recortadas/lanzallama.png';
+import lanzallamaimg from '../../assets/cartas_recortadas/lanzallama.png';
+import lacosaimg from '../../assets/cartas_recortadas/infectados/00.png';
+import infectadoimg from '../../assets/cartas_recortadas/infectados/01.png';
 
 const mapaCartas = {
-    back: backImage,
-    lanzallama: lanzallamaImage
+    'back': backImage,
+    'Lanzallamas': lanzallamaimg,
+    'La Cosa' : lacosaimg,
+    '¡Infectado!': infectadoimg
     // Agregar las cartas
 };
 
