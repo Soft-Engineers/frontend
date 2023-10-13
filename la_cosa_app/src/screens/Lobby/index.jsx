4e3f1de34dee9578921e83fe5a8b2cad7079c800
Benjamin Ceballos <benjamin.ceballos@mi.unc.edu.ar>
2023-10-13 16:37:09 -0300

    handle_socket_messages

--- la_cosa_app/src/screens/Lobby/index.jsx
+++ la_cosa_app/src/screens/Lobby/index.jsx
@@ -7,7 +7,7 @@ import RButton from "../../components/Button";
 import VideogameAssetOutlinedIcon from "@mui/icons-material/VideogameAssetOutlined";
 import { useNavigate } from "react-router-dom";
 import { useParams } from "react-router-dom";
-import { isHost  as checkIsHost, startMatch } from '../../utils/api'
+import { isHost as checkIsHost, startMatch } from '../../utils/api'
 
 const Lobby = () => {
     // State
@@ -32,7 +32,7 @@ const Lobby = () => {
             }
             else if (data.message_type === "start_match") {
                 navigate(`/match/${match_name}`);
-            } 
+            }
             else {
                 console.log('Mensaje no reconocido');
             }
@@ -49,26 +49,26 @@ const Lobby = () => {
 
     // Verificar si es el host
     useEffect(() => {
-      const reponse = checkIsHost (player_name, match_name);
-      reponse.then((data) => {
-        setIsHost(data.data.is_host);
-        console.log(isHost);
-      });
-      
+        const reponse = checkIsHost(player_name, match_name);
+        reponse.then((data) => {
+            setIsHost(data.data.is_host);
+            console.log(isHost);
+        });
+
     }, [player_name, match_name]);
 
     // Funcion para iniciar la partida
     const handleStartMatch = (player_name, match_name) => {
-        console.log({player_name, match_name});
-      const response = startMatch(player_name,match_name);
-      response.then((data) => {
-        console.log('DATA de startmatch',data.data);
-      });
+        console.log({ player_name, match_name });
+        const response = startMatch(player_name, match_name);
+        response.then((data) => {
+            console.log('DATA de startmatch', data.data);
+        });
     }
 
     return (
         <Container >
-            <Header/>
+            <Header />
             {/* Habra 2 mitades iguales divididas con Grid */}
             <Grid container spacing={2}>
                 {/* Primera mitad */}
@@ -79,15 +79,15 @@ const Lobby = () => {
                 </Grid>
                 {/* Segunda mitad */}
                 <Grid item xs={6} container justifyContent="center" alignItems="center">
-                  {isHost ? (
-                    <RButton
-                        text="Iniciar Partida"
-                        action={() => handleStartMatch(player_name, match_name)}
-                        icon={<VideogameAssetOutlinedIcon />}
-                    /> 
-                  ) : (
-                    <h1>Esperando que el host inicie la partida...</h1>
-                  )}
+                    {isHost ? (
+                        <RButton
+                            text="Iniciar Partida"
+                            action={() => handleStartMatch(player_name, match_name)}
+                            icon={<VideogameAssetOutlinedIcon />}
+                        />
+                    ) : (
+                        <h1>Esperando que el host inicie la partida...</h1>
+                    )}
                 </Grid>
             </Grid>
         </Container>
--- la_cosa_app/src/utils/api.js
+++ la_cosa_app/src/utils/api.js
@@ -1,4 +1,6 @@
 import axios from "axios";
+import { useEffect } from "react";
+import { useMatchC } from '../screens/Match/matchContext';
 
 // pasar como formdata a name_player
 export const createUser = async (name_player) => {
@@ -105,3 +107,82 @@ export const startMatch = async (player_name, match_name) => {
     throw error;
   }
 };
+
+export const handle_socket_messages = () => {
+  const { state, actions } = useMatchC();
+
+  const match_name = sessionStorage.getItem('match_name');
+  const player_name = sessionStorage.getItem('player_name');
+
+  console.log(match_name);
+  console.log(player_name);
+
+  useEffect(() => {
+    const matchSocket = new WebSocket(`ws://localhost:8000/ws/${match_name}/${player_name}`);
+    matchSocket.onopen = () => {
+      console.log("Conectado al socket de la partida");
+    };
+
+
+    matchSocket.onmessage = (event) => {
+      const data = JSON.parse(event.data);
+      switch (data.message_type) {
+        case "posiciones":
+          console.log('content de posiciones:', data.message_content);
+          actions.setJugadores(data.message_content);
+          break;
+        case 'estado inicial':
+          actions.setHand(data.message_content.hand);
+          if (data.message_content.current_turn === player_name) {
+            actions.setIsTurn(true);
+          } else {
+            actions.setIsTurn(false);
+          }
+          break;
+        case 'datos jugada':
+          if (data.message_content.turn === player_name) {
+            actions.setIsTurn(true);
+          } else {
+            actions.setIsTurn(false);
+          }
+          if (data.message_content.dead_player_name === player_name) {
+            actions.setDeadPlayer(true);
+          }
+          break;
+        case 'notificación muerte':
+        case 'notificación jugada':
+        case 'notificación turno':
+          actions.setAvisos([...state.avisos, data.message_content]);
+          break;
+        case 'partida finalizada':
+          actions.setWinners(data.message_content.winners);
+          actions.setReason(data.message_content.reason);
+          actions.setEndGame(true);
+          break;
+        case "cards":
+          console.log(data.message_content);
+          actions.setHand(data.message_content);
+          break;
+        case 'error':
+          actions.setSeverity('error');
+          actions.setBody(data.message_content);
+          actions.setOpen(true);
+          break;
+        default:
+          // Manejar otros tipos de mensajes si es necesario
+          break;
+      };
+    };
+    matchSocket.onclose = () => {
+      console.log("Desconectado del socket de la partida");
+    };
+
+    // Set the socket state
+    actions.setSocket(matchSocket);
+
+    return () => {
+      matchSocket.close();
+    };
+  }, [match_name]);
+
+}
