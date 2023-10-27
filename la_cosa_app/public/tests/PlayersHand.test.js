f71b71c7860017425c10da95ab861e4f3516cd51
Benjamin Ceballos <benjamin.ceballos@mi.unc.edu.ar>
2023-10-27 15:08:23 -0300

    PlayersHandTest

--- la_cosa_app/public/tests/PlayersHand.test.js
+++ la_cosa_app/public/tests/PlayersHand.test.js
@@ -1,22 +1,25 @@
 import { render, fireEvent, getAllByAltText } from '@testing-library/react';
+import '@testing-library/jest-dom';
 import PlayersHand from './../../src/components/PlayersHand';
-import { MatchProvider, useMatchC, state, actions } from '../../src/screens/Match/matchContext';
+import { MatchProvider, useMatchC } from '../../src/screens/Match/matchContext';
 import React from 'react';
 
-jest.mock('../../src/screens/Match/matchContext');
+jest.mock('../../src/screens/Match/matchContext', () => ({
+    ...jest.requireActual('../../src/screens/Match/matchContext'),
+    useMatchC: jest.fn(),
+}));
 
 describe('PlayersHand', () => {
 
 
     const mockContext = {
         state: {
-            //...state,
+
             currentTurn: 1,
-            hand: [{ card_name: 'La Cosa' }, { card_name: 'Sospecha' }, { card_name: 'Sospecha' }, { card_name: 'Sospecha' }],
+            hand: [{ card_name: 'La Cosa' }, { card_name: 'Sospecha' }, { card_name: 'Lanzallama' }, { card_name: 'Hacha' }],
             selectedCard: null,
         },
         actions: {
-            //...actions,
             setSelectedCard: jest.fn(),
         },
     };
@@ -33,28 +36,23 @@ describe('PlayersHand', () => {
 
     it('renderiza las cartas de la mano correctamente', () => {
 
-        const { state, actions } = useMatchC();
-        // Renderiza el componente
-        const { getAllByAltText } = render(
+        const { getByAltText } = render(
             <MatchProvider>
                 <PlayersHand />
             </MatchProvider>
         );
-        console.log("hand:", state.hand);
-        console.log("deadPlayer:", state.isDeadPlayer);
-
 
         // Asegúrate de que las cartas estén presentes en el render
         const carta1 = getByAltText(`Carta La Cosa`);
-        //const carta2 = getByAltText(`Carta Sospecha`);
-        //const carta3 = getByAltText(`Carta Sospecha`);
-        //const carta4 = getByAltText(`Carta Sospecha`);
+        const carta2 = getByAltText(`Carta Sospecha`);
+        const carta3 = getByAltText(`Carta Sospecha`);
+        const carta4 = getByAltText(`Carta Sospecha`);
 
 
         expect(carta1).toBeInTheDocument();
-        //expect(carta2).toBeInTheDocument();
-        //expect(carta3).toBeInTheDocument();
-        //expect(carta4).toBeInTheDocument();
+        expect(carta2).toBeInTheDocument();
+        expect(carta3).toBeInTheDocument();
+        expect(carta4).toBeInTheDocument();
 
     });
 });
