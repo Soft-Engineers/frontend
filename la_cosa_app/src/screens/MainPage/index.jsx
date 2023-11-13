480f6caf3ffdb851b9cd8db358863c6f330a3739
Benjamin Ceballos <benjamin.ceballos@mi.unc.edu.ar>
2023-11-13 20:48:32 -0300

    cambio listar partidas

--- la_cosa_app/src/screens/MainPage/index.jsx
+++ la_cosa_app/src/screens/MainPage/index.jsx
@@ -12,15 +12,14 @@ import FormPartida from "../../components/FormPartida";
 const styles = {
   root: {
     minHeight: "90vh",
-    minWidth: "100%",
   },
   header: {
-    height : "10vh",
+    height: "10vh",
   },
   Container: {
     display: "flex",
     flexDirection: "row",
-    height : "80vh",
+    height: "80vh",
   },
   buttonContainer: {
     display: "flex",
@@ -28,20 +27,21 @@ const styles = {
     flexDirection: "column",
     justifyContent: "flex-start",
     alignItems: "center",
-    marginTop: "1rem",
+    marginTop: "2rem",
+    marginLeft: "-5rem",
   },
   buttons: {
     display: "flex",
     width: "100%",
     flexDirection: "column",
     marginTop: '20vh',
-    gap: "30px",
+    gap: "20px",
     alignItems: "center",
   },
   table: {
     display: "flex",
     marginTop: "1rem",
-    minWidth: "50%",
+    minWidth: "60%",
   },
 };
 
@@ -58,43 +58,44 @@ const MainPage = () => {
   }, []);
 
   return (
-    <Container>
-      <Grid container spacing={0} sx={styles.root}>
-        <Grid item xs={12}>
-          <Header sx={styles.header} />
-        </Grid>
+    <div>
+      <Header sx={styles.header} />
+      <Container >
+        <Grid container spacing={5} sx={styles.root}>
 
-        <Grid item xs={12} sx={styles.Container}>
-          <Box sx={styles.buttonContainer}>
-            <h2> ¡Crea o uníte a una partida para empezar a jugar!</h2>
-            <Box sx={styles.buttons}>
-            {showForm ? (
-              <FormPartida />
-            ) : (
-              <RButton
-                text="Crear partida"
-                action={() => setShowForm(!showForm)}
-                icon={<VideogameAssetOutlinedIcon />}
-                sx={styles.button}
-              />
-            )}
-            <RButton
-              text="Recargar partidas"
-              icon={<RotateLeftOutlinedIcon />}
-              action={async () => {
-                const res = await getPartidas();
-                setPartidas(res);
-              }}
-              sx={styles.button}
-            />
-          </Box>
-        </Box>
-          <Box sx={styles.table}>
-            <Table data={partidas} />
-          </Box>
+          <Grid item xs={12} sx={styles.Container}>
+            <Box sx={styles.buttonContainer}>
+              <h2> ¡Crea o uníte a una partida </h2>
+              <h2>   para empezar a jugar!</h2>
+              <Box sx={styles.buttons}>
+                {showForm ? (
+                  <FormPartida />
+                ) : (
+                  <RButton
+                    text="Crear partida"
+                    action={() => setShowForm(!showForm)}
+                    icon={<VideogameAssetOutlinedIcon />}
+                    sx={styles.button}
+                  />
+                )}
+                <RButton
+                  text="Recargar partidas"
+                  icon={<RotateLeftOutlinedIcon />}
+                  action={async () => {
+                    const res = await getPartidas();
+                    setPartidas(res);
+                  }}
+                  sx={styles.button}
+                />
+              </Box>
+            </Box>
+            <Box sx={styles.table}>
+              <Table data={partidas} />
+            </Box>
+          </Grid>
         </Grid>
-      </Grid>
-    </Container>
+      </Container>
+    </div >
   );
 };
 
